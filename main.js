cf2d5a8c7378346e4636599ba20f54bbfb7638cc
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-16 16:16:17 +0530

    sdfa

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 const http = require('http');
 
+const fs=require("fs");
+
+const server=http.createServer((request,response)=>{
+
+fs.readFile('./output.txt',(error,data)=>{
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log("Hello, World!");
+    }
+
+})
+response.end();
+})
+
+
 // TODO: Create an HTTP server
 
 // TODO: Listen on port 3000
